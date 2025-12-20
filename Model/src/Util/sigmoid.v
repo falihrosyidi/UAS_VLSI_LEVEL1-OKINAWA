20b8cef356a51c9535b9551e41727d5c8b6b5081
Frozyid <108382141+falihrosyidi@users.noreply.github.com>
2025-12-20 12:58:39 +0700

    add: neuron_c.v and sigmoid.v empty file

---
+++ Model/src/Util/sigmoid.v
@@ -0,0 +1,13 @@
+`ifndef SIGMOID
+`define SIGMOID
+
+module sigmoid #(
+    parameter WIDTH = 32
+) (
+    input [WIDTH-1:0] a,
+    output [WIDTH-1:0] y
+);
+    // algoritma SIGMOID
+endmodule
+
+`endif 
