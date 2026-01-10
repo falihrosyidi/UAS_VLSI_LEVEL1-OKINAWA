3d2c7b2c30f722881f64b0bf4edb30886221a68c
AbdAziz12 <abdaziz1215@gmail.com>
2026-01-10 18:15:51 +0700

    fix: weight & bias value

--- Model/src/b_mem.v
+++ Model/src/b_mem.v
@@ -27,33 +27,33 @@ module b_mem #(
     
     // Bias Generator Layer 2 (bg2) - Choice 0
     wire signed [WIDTH-1:0] b0_g_l2 [N_G_L2-1:0];
-    assign b0_g_l2[0] = 32'h01A1B251; // 1.631627
-    assign b0_g_l2[1] = 32'h00EF37E3; // 0.934426
-    assign b0_g_l2[2] = 32'h0041432E; // 0.254929
+    assign b0_g_l2[0] = 32'h01A1B252; // 1.631627202
+    assign b0_g_l2[1] = 32'h00EF368B; // 0.93442601
+    assign b0_g_l2[2] = 32'h00414304; // 0.254928827
     assign bg2_0 = {b0_g_l2[2], b0_g_l2[1], b0_g_l2[0]};
 
     // Bias Generator Layer 3 (bg3) - Choice 0
     wire signed [WIDTH-1:0] b0_g_l3 [N_G_L3-1:0];
-    assign b0_g_l3[0] = 32'h0163E32C; // 1.390187
-    assign b0_g_l3[1] = 32'h014AAB54; // 1.291887
-    assign b0_g_l3[2] = 32'h010BC5C6; // 1.046107
-    assign b0_g_l3[3] = 32'h0111295A; // 1.067029
-    assign b0_g_l3[4] = 32'hFEB9FAAD; // -1.2728
-    assign b0_g_l3[5] = 32'h01106A9F; // 1.064147
-    assign b0_g_l3[6] = 32'h014D351F; // 1.301596
-    assign b0_g_l3[7] = 32'h0146C65A; // 1.276455
-    assign b0_g_l3[8] = 32'h01374BD8; // 1.216003
+    assign b0_g_l3[0] = 32'h0163E347; // 1.390186727
+    assign b0_g_l3[1] = 32'h014AB91D; // 1.291887105
+    assign b0_g_l3[2] = 32'h010BCDA6; // 1.046106696
+    assign b0_g_l3[3] = 32'h011128D2; // 1.067029119
+    assign b0_g_l3[4] = 32'hFEBA2999; // -1.27280277
+    assign b0_g_l3[5] = 32'h01106BEB; // 1.064146698
+    assign b0_g_l3[6] = 32'h014D3565; // 1.301595986
+    assign b0_g_l3[7] = 32'h0146C5C1; // 1.276454985
+    assign b0_g_l3[8] = 32'h01374BFC; // 1.21600318
     assign bg3_0 = {b0_g_l3[8], b0_g_l3[7], b0_g_l3[6], b0_g_l3[5], b0_g_l3[4], b0_g_l3[3], b0_g_l3[2], b0_g_l3[1], b0_g_l3[0]};
 
     // Bias Discriminator Layer 2 (bd2) - Choice 0
     wire signed [WIDTH-1:0] b0_d_l2 [N_D_L2-1:0];
-    assign b0_d_l2[0] = 32'h025346B1; // 2.325301
-    assign b0_d_l2[1] = 32'h0195463D; // 1.583088
-    assign b0_d_l2[2] = 32'hFEE8F371; // -1.0901
+    assign b0_d_l2[0] = 32'h025346EE; // 2.325301051
+    assign b0_d_l2[1] = 32'h01954545; // 1.583088219
+    assign b0_d_l2[2] = 32'hFEE8EF7B; // -1.090095818
     assign bd2_0 = {b0_d_l2[2], b0_d_l2[1], b0_d_l2[0]};
 
     // Bias Discriminator Layer 3 (bd3) - Choice 0
-    assign bd3_0 = 32'hFF0AB782; // -0.95783
+    assign bd3_0 = 32'hFF0ACBCD; // -0.957827747
 
     // ========================================================
     // DATA SET 1 (choice == 1)
@@ -61,33 +61,33 @@ module b_mem #(
     
     // Bias Generator Layer 2 (bg2) - Choice 1
     wire signed [WIDTH-1:0] b1_g_l2 [N_G_L2-1:0];
-    assign b1_g_l2[0] = 32'h004FF5B6; // 0.312248
-    assign b1_g_l2[1] = 32'h013211EC; // 1.19559
-    assign b1_g_l2[2] = 32'hFEC6FD9C; // -1.2229
+    assign b1_g_l2[0] = 32'h004FEF7D; // 0.312248051
+    assign b1_g_l2[1] = 32'h01321232; // 1.195590138
+    assign b1_g_l2[2] = 32'hFEC6EFD6; // -1.222902894
     assign bg2_1 = {b1_g_l2[2], b1_g_l2[1], b1_g_l2[0]};
 
     // Bias Generator Layer 3 (bg3) - Choice 1
     wire signed [WIDTH-1:0] b1_g_l3 [N_G_L3-1:0];
-    assign b1_g_l3[0] = 32'hFE97BA14; // -1.40726
-    assign b1_g_l3[1] = 32'h01777266; // 1.466492
-    assign b1_g_l3[2] = 32'hFE7DF6F6; // -1.5057
-    assign b1_g_l3[3] = 32'h014A62AC; // 1.290558
-    assign b1_g_l3[4] = 32'h015EBA5B; // 1.369977
-    assign b1_g_l3[5] = 32'h014F75DB; // 1.310394
-    assign b1_g_l3[6] = 32'hFEADED29; // -1.32089
-    assign b1_g_l3[7] = 32'h015452B8; // 1.329336
-    assign b1_g_l3[8] = 32'hFE6E7E6F; // -1.56614
+    assign b1_g_l3[0] = 32'hFE97BDF2; // -1.407257915
+    assign b1_g_l3[1] = 32'h01776C06; // 1.466492057
+    assign b1_g_l3[2] = 32'hFE7E8A8C; // -1.505698442
+    assign b1_g_l3[3] = 32'h014A61FF; // 1.290557802
+    assign b1_g_l3[4] = 32'h015EB6CC; // 1.369976759
+    assign b1_g_l3[5] = 32'h014F75F3; // 1.310393512
+    assign b1_g_l3[6] = 32'hFEADD9E2; // -1.320894122
+    assign b1_g_l3[7] = 32'h01544F63; // 1.329336345
+    assign b1_g_l3[8] = 32'hFE6F117B; // -1.566139519
     assign bg3_1 = {b1_g_l3[8], b1_g_l3[7], b1_g_l3[6], b1_g_l3[5], b1_g_l3[4], b1_g_l3[3], b1_g_l3[2], b1_g_l3[1], b1_g_l3[0]};
 
     // Bias Discriminator Layer 2 (bd2) - Choice 1
     wire signed [WIDTH-1:0] b1_d_l2 [N_D_L2-1:0];
-    assign b1_d_l2[0] = 32'hFF223F6F; // -0.86624
-    assign b1_d_l2[1] = 32'hFCFA3A3D; // -3.0216
-    assign b1_d_l2[2] = 32'h00060D6D; // 0.023642
+    assign b1_d_l2[0] = 32'hFF223DCC; // -0.866244555
+    assign b1_d_l2[1] = 32'hFCFA7869; // -3.021600187
+    assign b1_d_l2[2] = 32'h00060D6F; // 0.02364248
     assign bd2_1 = {b1_d_l2[2], b1_d_l2[1], b1_d_l2[0]};
 
     // Bias Discriminator Layer 3 (bd3) - Choice 1
-    assign bd3_1 = 32'hFF43FC67; // -0.73444
+    assign bd3_1 = 32'hFF43FBB5; // -0.734440506
 
     // ========================================================
     // LOGIKA PEMILIH (MULTIPLEXER)
--- Model/src/w_mem.v
+++ Model/src/w_mem.v
@@ -28,22 +28,22 @@ module w_mem #(
     
     // WG2 - Choice 0
     wire signed [WIDTH-1:0] g0_l2 [2:0][1:0];
-    assign g0_l2[0][0] = 32'hFFFF136B; assign g0_l2[0][1] = 32'h0003B16C;
-    assign g0_l2[1][0] = 32'h0011018B; assign g0_l2[1][1] = 32'h0003A141;
-    assign g0_l2[2][0] = 32'hFFCD02A7; assign g0_l2[2][1] = 32'hFFE2AB07;
+    assign g0_l2[0][0] = 32'hFFFF1337; assign g0_l2[0][1] = 32'h0003B169;
+    assign g0_l2[1][0] = 32'h0011008F; assign g0_l2[1][1] = 32'h0003A13E;
+    assign g0_l2[2][0] = 32'hFFCCFC5C; assign g0_l2[2][1] = 32'hFFE2AD20;
     assign wg2_0 = {g0_l2[2][1], g0_l2[2][0], g0_l2[1][1], g0_l2[1][0], g0_l2[0][1], g0_l2[0][0]};
 
     // WG3 - Choice 0
     wire signed [WIDTH-1:0] g0_l3 [8:0][2:0];
-    assign g0_l3[0][0]=32'h00A48E44; assign g0_l3[0][1]=32'h004EF36E; assign g0_l3[0][2]=32'h0022E4D2;
-    assign g0_l3[1][0]=32'h00C1262B; assign g0_l3[1][1]=32'h005406F8; assign g0_l3[1][2]=32'h002FF424;
-    assign g0_l3[2][0]=32'h00E854FD; assign g0_l3[2][1]=32'h006E334D; assign g0_l3[2][2]=32'h00272295;
-    assign g0_l3[3][0]=32'h00EDF458; assign g0_l3[3][1]=32'h007302EE; assign g0_l3[3][2]=32'h000F7B18;
-    assign g0_l3[4][0]=32'hFF19519A; assign g0_l3[4][1]=32'hFFC7C0B0; assign g0_l3[4][2]=32'hFFFEE823;
-    assign g0_l3[5][0]=32'h01015B70; assign g0_l3[5][1]=32'h006460D1; assign g0_l3[5][2]=32'h000431D0;
-    assign g0_l3[6][0]=32'h00D41A06; assign g0_l3[6][1]=32'h003D0464; assign g0_l3[6][2]=32'h002C355C;
-    assign g0_l3[7][0]=32'h00C7D339; assign g0_l3[7][1]=32'h006B3EFC; assign g0_l3[7][2]=32'hFFFDB8B6;
-    assign g0_l3[8][0]=32'h00C21C3D; assign g0_l3[8][1]=32'h007A1E0E; assign g0_l3[8][2]=32'hFFFFC4F4;
+    assign g0_l3[0][0]=32'h00A48E60; assign g0_l3[0][1]=32'h004EE8B7; assign g0_l3[0][2]=32'h0022E4B6;
+    assign g0_l3[1][0]=32'h00C125C5; assign g0_l3[1][1]=32'h005407FC; assign g0_l3[1][2]=32'h002FF250;
+    assign g0_l3[2][0]=32'h00E8551F; assign g0_l3[2][1]=32'h006E32DF; assign g0_l3[2][2]=32'h0027230D;
+    assign g0_l3[3][0]=32'h00EDF470; assign g0_l3[3][1]=32'h0073039C; assign g0_l3[3][2]=32'h000F7B9E;
+    assign g0_l3[4][0]=32'hFF195298; assign g0_l3[4][1]=32'hFFC7C15B; assign g0_l3[4][2]=32'hFFEF8040;
+    assign g0_l3[5][0]=32'h01015BDF; assign g0_l3[5][1]=32'h00646145; assign g0_l3[5][2]=32'h000431B4;
+    assign g0_l3[6][0]=32'h00D41B22; assign g0_l3[6][1]=32'h003D0432; assign g0_l3[6][2]=32'h002C3587;
+    assign g0_l3[7][0]=32'h00C7D3B9; assign g0_l3[7][1]=32'h006B3E87; assign g0_l3[7][2]=32'hFFFD96F5;
+    assign g0_l3[8][0]=32'h00C21CDB; assign g0_l3[8][1]=32'h007A1D7C; assign g0_l3[8][2]=32'hFFFEC42F;
     assign wg3_0 = {g0_l3[8][2], g0_l3[8][1], g0_l3[8][0], g0_l3[7][2], g0_l3[7][1], g0_l3[7][0], 
                     g0_l3[6][2], g0_l3[6][1], g0_l3[6][0], g0_l3[5][2], g0_l3[5][1], g0_l3[5][0],
                     g0_l3[4][2], g0_l3[4][1], g0_l3[4][0], g0_l3[3][2], g0_l3[3][1], g0_l3[3][0],
@@ -52,15 +52,15 @@ module w_mem #(
 
     // WD2 - Choice 0
     wire signed [WIDTH-1:0] d0_l2 [2:0][8:0];
-    assign d0_l2[0][0]=32'hFFD27D37; assign d0_l2[0][1]=32'hFFCC5E1D; assign d0_l2[0][2]=32'hFFA019D2; assign d0_l2[0][3]=32'hFFB55620; assign d0_l2[0][4]=32'h00514D23; assign d0_l2[0][5]=32'hFFB6BAA0; assign d0_l2[0][6]=32'hFFBAEEA1; assign d0_l2[0][7]=32'hFFA8AA59; assign d0_l2[0][8]=32'hFFC8F99D;
-    assign d0_l2[1][0]=32'hFFA86CA1; assign d0_l2[1][1]=32'hFFC71FA2; assign d0_l2[1][2]=32'hFFDE1B7D; assign d0_l2[1][3]=32'hFFD2040E; assign d0_l2[1][4]=32'h00313C5D; assign d0_l2[1][5]=32'hFFD8DB02; assign d0_l2[1][6]=32'hFFB3544D; assign d0_l2[1][7]=32'hFFED1B04; assign d0_l2[1][8]=32'hFFD11139;
-    assign d0_l2[2][0]=32'h0035798E; assign d0_l2[2][1]=32'h0045D446; assign d0_l2[2][2]=32'h0017F38F; assign d0_l2[2][3]=32'h00185A48; assign d0_l2[2][4]=32'hFFE1F21C; assign d0_l2[2][5]=32'h00221C2B; assign d0_l2[2][6]=32'h000BF9FD; assign d0_l2[2][7]=32'h001DECCB; assign d0_l2[2][8]=32'h0041FB6E;
+    assign d0_l2[0][0]=32'hFFD27C89; assign d0_l2[0][1]=32'hFFCC5DD9; assign d0_l2[0][2]=32'hFF9FB305; assign d0_l2[0][3]=32'hFFB55605; assign d0_l2[0][4]=32'h00514D79; assign d0_l2[0][5]=32'hFFB6BB88; assign d0_l2[0][6]=32'hFFBACC69; assign d0_l2[0][7]=32'hFFA8A90A; assign d0_l2[0][8]=32'hFFC8FA23;
+    assign d0_l2[1][0]=32'hFFA86C59; assign d0_l2[1][1]=32'hFFC71F2D; assign d0_l2[1][2]=32'hFFDE1A97; assign d0_l2[1][3]=32'hFFD2036E; assign d0_l2[1][4]=32'h00313C80; assign d0_l2[1][5]=32'hFFD8DA61; assign d0_l2[1][6]=32'hFFB354D2; assign d0_l2[1][7]=32'hFFED1AF9; assign d0_l2[1][8]=32'hFFD11093;
+    assign d0_l2[2][0]=32'h00357A66; assign d0_l2[2][1]=32'h0045D529; assign d0_l2[2][2]=32'h0017F558; assign d0_l2[2][3]=32'h00185AAB; assign d0_l2[2][4]=32'hFFE1F28F; assign d0_l2[2][5]=32'h00221BA0; assign d0_l2[2][6]=32'h000BFCA2; assign d0_l2[2][7]=32'h001DEA80; assign d0_l2[2][8]=32'h0041FC2B;
     assign wd2_0 = {d0_l2[2][8], d0_l2[2][7], d0_l2[2][6], d0_l2[2][5], d0_l2[2][4], d0_l2[2][3], d0_l2[2][2], d0_l2[2][1], d0_l2[2][0],
                     d0_l2[1][8], d0_l2[1][7], d0_l2[1][6], d0_l2[1][5], d0_l2[1][4], d0_l2[1][3], d0_l2[1][2], d0_l2[1][1], d0_l2[1][0],
                     d0_l2[0][8], d0_l2[0][7], d0_l2[0][6], d0_l2[0][5], d0_l2[0][4], d0_l2[0][3], d0_l2[0][2], d0_l2[0][1], d0_l2[0][0]};
 
     // WD3 - Choice 0
-    assign wd3_0 = {32'h01537752, 32'hFE2AF3B2, 32'hFD5761CC};
+    assign wd3_0 = {32'h015376DF, 32'hFE2AFC21, 32'hFD577081};
 
     // ========================================================
     // DATA SET 1 (choice == 1)
@@ -68,22 +68,22 @@ module w_mem #(
 
     // WG2 - Choice 1
     wire signed [WIDTH-1:0] g1_l2 [2:0][1:0];
-    assign g1_l2[0][0] = 32'h00031B23; assign g1_l2[0][1] = 32'h000B1F2D;
-    assign g1_l2[1][0] = 32'h0006698C; assign g1_l2[1][1] = 32'hFFF75574;
-    assign g1_l2[2][0] = 32'hFFF58F6B; assign g1_l2[2][1] = 32'hFFF7F06B;
+    assign g1_l2[0][0] = 32'h00031B20; assign g1_l2[0][1] = 32'h000B1F2B;
+    assign g1_l2[1][0] = 32'h00066991; assign g1_l2[1][1] = 32'hFFFF7596;
+    assign g1_l2[2][0] = 32'hFFF58EEF; assign g1_l2[2][1] = 32'hFFF7F0E9;
     assign wg2_1 = {g1_l2[2][1], g1_l2[2][0], g1_l2[1][1], g1_l2[1][0], g1_l2[0][1], g1_l2[0][0]};
 
     // WG3 - Choice 1
     wire signed [WIDTH-1:0] g1_l3 [8:0][2:0];
-    assign g1_l3[0][0]=32'hFFD8A3B2; assign g1_l3[0][1]=32'hFF8B0692; assign g1_l3[0][2]=32'h0080057B;
-    assign g1_l3[1][0]=32'h00276D3B; assign g1_l3[1][1]=32'h0084AA00; assign g1_l3[1][2]=32'hFF8E9C68;
-    assign g1_l3[2][0]=32'h003B3597; assign g1_l3[2][1]=32'hFF9EB60C; assign g1_l3[2][2]=32'h009F19E5;
-    assign g1_l3[3][0]=32'h00608560; assign g1_l3[3][1]=32'h009453C9; assign g1_l3[3][2]=32'hFF84E07C;
-    assign g1_l3[4][0]=32'hFFFA398F; assign g1_l3[4][1]=32'h00A091A4; assign g1_l3[4][2]=32'hFF86FA74;
-    assign g1_l3[5][0]=32'h00676C25; assign g1_l3[5][1]=32'h007E69C1; assign g1_l3[5][2]=32'hFF7B9E2B;
-    assign g1_l3[6][0]=32'hFFF75647; assign g1_l3[6][1]=32'hFF6F2E23; assign g1_l3[6][2]=32'h008BFA91;
-    assign g1_l3[7][0]=32'h0019827C; assign g1_l3[7][1]=32'h0082F05D; assign g1_l3[7][2]=32'hFF6E941E;
-    assign g1_l3[8][0]=32'hFFF0F9B3; assign g1_l3[8][1]=32'hFF99684C; assign g1_l3[8][2]=32'h007A5F49;
+    assign g1_l3[0][0]=32'hFFD8A468; assign g1_l3[0][1]=32'hFF8B0939; assign g1_l3[0][2]=32'h0080057C;
+    assign g1_l3[1][0]=32'h00276CBE; assign g1_l3[1][1]=32'h0084AA0D; assign g1_l3[1][2]=32'hFF8DF7FE;
+    assign g1_l3[2][0]=32'h003B3552; assign g1_l3[2][1]=32'hFF9EC001; assign g1_l3[2][2]=32'h009F1DD0;
+    assign g1_l3[3][0]=32'h0060857C; assign g1_l3[3][1]=32'h00945439; assign g1_l3[3][2]=32'hFF84DF8D;
+    assign g1_l3[4][0]=32'hFFFA371B; assign g1_l3[4][1]=32'h00A0914C; assign g1_l3[4][2]=32'hFF86F833;
+    assign g1_l3[5][0]=32'h00676C3E; assign g1_l3[5][1]=32'h007E69DA; assign g1_l3[5][2]=32'hFF7BA2C3;
+    assign g1_l3[6][0]=32'hFFF75605; assign g1_l3[6][1]=32'hFF6F7B85; assign g1_l3[6][2]=32'h008BF826;
+    assign g1_l3[7][0]=32'h00198217; assign g1_l3[7][1]=32'h0082EDA4; assign g1_l3[7][2]=32'hFF6E93CE;
+    assign g1_l3[8][0]=32'hFFF0EC5F; assign g1_l3[8][1]=32'hFF996709; assign g1_l3[8][2]=32'h007A5F7B;
     assign wg3_1 = {g1_l3[8][2], g1_l3[8][1], g1_l3[8][0], g1_l3[7][2], g1_l3[7][1], g1_l3[7][0], 
                     g1_l3[6][2], g1_l3[6][1], g1_l3[6][0], g1_l3[5][2], g1_l3[5][1], g1_l3[5][0],
                     g1_l3[4][2], g1_l3[4][1], g1_l3[4][0], g1_l3[3][2], g1_l3[3][1], g1_l3[3][0],
@@ -92,15 +92,15 @@ module w_mem #(
 
     // WD2 - Choice 1
     wire signed [WIDTH-1:0] d1_l2 [2:0][8:0];
-    assign d1_l2[0][0]=32'hFFE5176F; assign d1_l2[0][1]=32'h002ACDB6; assign d1_l2[0][2]=32'hFFB96EA0; assign d1_l2[0][3]=32'h001F624C; assign d1_l2[0][4]=32'h001D28C4; assign d1_l2[0][5]=32'h002C605D; assign d1_l2[0][6]=32'hFFEEF697; assign d1_l2[0][7]=32'h001107FD; assign d1_l2[0][8]=32'hFFEB1EA0;
-    assign d1_l2[1][0]=32'hFF9AE543; assign d1_l2[1][1]=32'h00544062; assign d1_l2[1][2]=32'hFFA89D42; assign d1_l2[1][3]=32'h004F28B0; assign d1_l2[1][4]=32'h0056B7D5; assign d1_l2[1][5]=32'h00516F71; assign d1_l2[1][6]=32'hFFA1517F; assign d1_l2[1][7]=32'h005F014E; assign d1_l2[1][8]=32'hFF9CED09;
-    assign d1_l2[2][0]=32'h002979CF; assign d1_l2[2][1]=32'h001DF270; assign d1_l2[2][2]=32'hFFFFEFBD; assign d1_l2[2][3]=32'hFFE4FD91; assign d1_l2[2][4]=32'h000AF59C; assign d1_l2[2][5]=32'hFFE51CAD; assign d1_l2[2][6]=32'hFFE87560; assign d1_l2[2][7]=32'hFFE76A23; assign d1_l2[2][8]=32'h0021AEEA;
+    assign d1_l2[0][0]=32'hFFE5178C; assign d1_l2[0][1]=32'h002ACB79; assign d1_l2[0][2]=32'hFFB96ECB; assign d1_l2[0][3]=32'h001F634E; assign d1_l2[0][4]=32'h001D28AA; assign d1_l2[0][5]=32'h002C5FEA; assign d1_l2[0][6]=32'hFFEDF254; assign d1_l2[0][7]=32'h0011072F; assign d1_l2[0][8]=32'hFFEB1E02;
+    assign d1_l2[1][0]=32'hFF9AEABE; assign d1_l2[1][1]=32'h005440AF; assign d1_l2[1][2]=32'hFFA89D14; assign d1_l2[1][3]=32'h004F2882; assign d1_l2[1][4]=32'h0056B7E0; assign d1_l2[1][5]=32'h00516F24; assign d1_l2[1][6]=32'hFFA15139; assign d1_l2[1][7]=32'h005F00DF; assign d1_l2[1][8]=32'hFF9CF80C;
+    assign d1_l2[2][0]=32'h002979E8; assign d1_l2[2][1]=32'h001DEE50; assign d1_l2[2][2]=32'hFFFEF9A6; assign d1_l2[2][3]=32'hFFE4FD36; assign d1_l2[2][4]=32'h000AF499; assign d1_l2[2][5]=32'hFFE51CBA; assign d1_l2[2][6]=32'hFFE8754C; assign d1_l2[2][7]=32'hFFE76A07; assign d1_l2[2][8]=32'h0021AE47;
     assign wd2_1 = {d1_l2[2][8], d1_l2[2][7], d1_l2[2][6], d1_l2[2][5], d1_l2[2][4], d1_l2[2][3], d1_l2[2][2], d1_l2[2][1], d1_l2[2][0],
                     d1_l2[1][8], d1_l2[1][7], d1_l2[1][6], d1_l2[1][5], d1_l2[1][4], d1_l2[1][3], d1_l2[1][2], d1_l2[1][1], d1_l2[1][0],
                     d1_l2[0][8], d1_l2[0][7], d1_l2[0][6], d1_l2[0][5], d1_l2[0][4], d1_l2[0][3], d1_l2[0][2], d1_l2[0][1], d1_l2[0][0]};
 
     // WD3 - Choice 1
-    assign wd3_1 = {32'hFFE0CB20, 32'h03811853, 32'h012918AC};
+    assign wd3_1 = {32'hFFE0CB4D, 32'h03811939, 32'h012919AA};
 
     // ========================================================
     // FINAL OUTPUT SELECTION (MUX)
